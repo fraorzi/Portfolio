4e9eee730784af3645af1d71a2b56172617eca49
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-01-17 21:21:07 +0100

    Added imports and components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { Navbar } from './components/NavBar/NavBar';
+import { Banner } from './components/Banner/Banner';
+import { Skills } from './components/Skills/Skills';
+import { Projects } from './components/Projects/Projects';
+import { Contact } from './components/Contact/Contact';
+import { Footer } from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
