059a74928a5e4100d722c846051d64fe873101d3
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-03-25 18:53:58 +0100

    minor changes to css and app.js, 1 added line in gitignore

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from './logo.svg';
 import './App.css';
-import { Navbar } from './components/NavBar/NavBar';
-import { Banner } from './components/Banner/Banner';
-import { Skills } from './components/Skills/Skills';
-import { Projects } from './components/Projects/Projects';
-import { Contact } from './components/Contact/Contact';
-import { Footer } from './components/Footer/Footer';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavBar />
       <Banner />
       <Skills />
       <Projects />
