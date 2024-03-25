44e1b6693a9453e78e443077fb878ab44d8301db
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-03-25 20:31:39 +0100

    fixed wrong imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+import logo from './assets/img/logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Banner } from "./components/Banner/Banner";
+import { Skills } from "./components/Skills/Skills";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
