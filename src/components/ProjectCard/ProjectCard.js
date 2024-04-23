4c19d44d67f491838927acbdab2ffdb8de2d1805
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-04-23 04:54:17 +0200

    adjusted component for ability to use links to projects

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,15 +1,23 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, githubUrl }) => {
+  const handleClick = () => {
+    if (githubUrl) {
+      window.open(githubUrl, "_blank");
+    }
+  };
+
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <div className="project-link" onClick={handleClick} style={{ cursor: githubUrl ? 'pointer' : 'default' }}>
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt={title} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
