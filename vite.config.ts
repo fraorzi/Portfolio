aade3da85d4abc050c485a29d0e958a0d5bbb7c6
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2025-06-21 00:51:07 +0200

    chore: add and configure TS and Vite path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
+  },
 });
